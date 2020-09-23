088cf0f2e29c7d3fc895922024f7dcb1e88091c0
Wagner <wagner.rua@hotmail.com>
2020-09-23 19:29:04 +0200

    Port config

--- src/index.js
+++ src/index.js
@@ -13,4 +13,4 @@ app.use(bodyParser.urlencoded({ extended: false }));
 require('./controllers/authController')(app);
 require('./controllers/robotController')(app);
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
